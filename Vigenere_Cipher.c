32c14ba11fc656a46fb346e696c8b6e7b06c1e9f
Daniel de Souza Lima <39025412+DeWaldman@users.noreply.github.com>
2020-02-10 18:07:50 -0300

    Update Vigenere_Cipher.c

--- Vigenere_Cipher.c
+++ Vigenere_Cipher.c
@@ -3,21 +3,35 @@
 #define alpha "abcdefghijklmnopqrstuvwxyz"//definindo o alphabeto de referencia;
 void crip(char *msg, char *pwd, char *cmsg);//criptografa a mensagem;
 void desc(char *msg, char *pwd, char *cmsg);//descriptografa a mensagem;
-int find(char *x, char y);//acha o index do elemento em uma string, retorna -1 caso nao ache;
+int find_alpha(char x);//acha o index do elemento em alpha, retorna -1 caso nao ache;
 
-int find(char *x, char y){
+int find_alpha(char x){
   int index;
-  for(index=0;index<strlen(&x[0]);index++){
-  	if(x[index] == y){
+  for(index=0;index<26;index++){
+  	if(alpha[index] == x){
       return index;
 	}
   }
   return -1;
 }
 
-void crip(char *msg, char *pwd, char *cmsg){
-  
+void crip(char *_msg, char *_pwd, char *_cmsg){
+  int a;
+  for(a=0;a<strlen(&_pwd[0]);a++){
+  	if(a==strlen(&_pwd[0])){break;}
+  &_cmsg[0] = 'a'//alpha[(find_alpha(&_msg[a])+find_alpha(&_pwd[a]))%26];
+  printf("%c\n", &_cmsg[a]);
+  }
 }
+
+/*void desc(char *msg, char *pwd, char *cmsg){
+  int a;
+  for(a=0;a<strlen(*pwd);a++){
+  	if(a==strlen(*pwd)){break;}
+	cmsg[a] = alpha[(find_alpha(*msg[a])-find_alpha(*pwd[a]))%26];
+  }
+}*/
+
 int main(void) {
   int a;//contadores
   int user;//variaveis de selecao
@@ -25,15 +39,26 @@ int main(void) {
   for(a=0;a<300;a++){msg[a]=0;pwd[a]=0;cmsg[a]=0;}//zerando os valores aleatorios dos vetores;
   
   do{
-    printf("(1)Criptografar (2)Descriptografar");scanf("%d", &user);//escolhendo o caminho;
-  }while(user != 1 || user != 2)
+    printf("(1)Criptografar (2)Descriptografar\n");scanf("%d", &user);//escolhendo o caminho;
+  }while(user != 1 && user != 2);
+  
+  printf("msg e pwd:\n");
   scanf("%s", &msg[0]);scanf("%s", &pwd[0]);//recebendo mensagem e senha;
   
+  //______________________________
+  a=0;
+  while(strlen(msg)>strlen(pwd)){ // Verificacao: a senha deve ser >= que a mensagem
+  	pwd[strlen(pwd)] = pwd[a];
+	a++;
+  }
+  //______________________________
+  
   if(user==1){
-    crip(msg, pwd);	
+    crip(&msg[0], &pwd[0], &cmsg[0]);
+    printf("%s\n",cmsg);
   }
   else{
-  	desc(msg, pwd);
+  	//desc(&msg[0], &pwd[0], &cmsg[0]);
   }
   return 0;
 }
